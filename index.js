ce0690e829062b22baf74b350415d60401be152d
Vinicius Benedito <viniciusaugustobenedito@gmail.com>
2026-02-02 01:09:54 -0300

    Utilização da lib 'ejs' para exibir uma view com os dados armazenados no banco. Implementação de conexão com o banco mongodb através da lib 'mongoose', criando um 'schema' nesse processo. Criação de endpoints básicos.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const { Person } = require("./person");
+// const { Person } = require("./person");
 
-// require("./modules/path");
-// require("./modules/fs");
-// require("./modules/http");
-require("./modules/express");
+const dotenv = require('dotenv');
 
-const person = new Person("Vinicius");
+const connectToDatabase = require('./src/database/connect');
+
+dotenv.config();
+
+connectToDatabase();
+
+// require('./modules/path');
+// require('./modules/fs');
+// require('./modules/http');
+require('./modules/express');
+
+// const person = new Person("Vinicius");
 // console.log(person.sayMyName());
